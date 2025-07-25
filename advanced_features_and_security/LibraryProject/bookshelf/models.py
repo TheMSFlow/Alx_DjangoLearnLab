ddea5e02022e77734a13981a7cfbff57425e1fd4
Michael <themichaelsteve@gmail.com>
2025-07-25 11:10:53 +0100

    Task 0 edit 2

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -43,3 +43,9 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
+
+    class Meta:
+        permissions = [
+            ("can_create", "Can create book"),
+            ("can_delete", "Can delete book"),
+        ]
