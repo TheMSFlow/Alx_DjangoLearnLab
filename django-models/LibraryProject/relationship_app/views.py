a12bc3ad9e4a070def1048cda030949c71fac07c
Michael <themichaelsteve@gmail.com>
2025-07-25 09:04:47 +0100

    Task 1 edits. 3

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,10 +6,10 @@ from .models import Library
 # ✅ Function-based view that uses the correct template path and query
 def list_books(request):
     books = Book.objects.all()  # ✅ Required line
-    return render(request, 'relationship_app/templates/list_books.html', {'books': books})  # ✅ Required path
+    return render(request, 'relationship_app/list_books.html', {'books': books})  # ✅ Required path
 
 # ✅ Class-based view for library details
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/templates/library_detail.html'  # Match template location
+    template_name = 'relationship_app/library_detail.html'  # Match template location
     context_object_name = 'library'
