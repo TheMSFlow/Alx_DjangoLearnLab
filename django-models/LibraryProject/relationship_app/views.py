472c3ff6ca1a24acb19049b109693deb13819064
Michael <themichaelsteve@gmail.com>
2025-07-25 09:17:29 +0100

    TAsk 2 edit

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -16,7 +16,8 @@ class LibraryDetailView(DetailView):
 
 
 from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
 # Login View
